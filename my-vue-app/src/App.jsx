e115b38c8af6690e77050e8be0889735308853df
Chai <developer.chaitra@gmail.com>
2023-07-17 00:57:47 +1200

    First Commit

---
+++ my-vue-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./Components/Home/index.jsx";
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-vue-app/src/Components/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className='absolute -translate-y-2/4 w-2/5 max-h-[90%] left-[10%] top-2/4'>
+      <h1>Hi! I am Chaitra.</h1>
+    </div>
+  );
+};
+
+export default Home;
